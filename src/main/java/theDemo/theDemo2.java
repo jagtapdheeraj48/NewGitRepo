6c9bbe435a3241d105d7eec5fc9593e0202969b6
jagtapdheeraj48 <135323345+jagtapdheeraj48@users.noreply.github.com>
2023-06-04 18:14:42 +0530

    Update theDemo2.java
    one two

--- src/main/java/theDemo/theDemo2.java
+++ src/main/java/theDemo/theDemo2.java
@@ -5,7 +5,7 @@ public class theDemo2 {
 	public static void main(String[] args) {
 System.out.println("theDemo 2");
  System.out.println("addition");
- System.out.println("one more");
+  System.out.println("one two");
 	}
 
 }
